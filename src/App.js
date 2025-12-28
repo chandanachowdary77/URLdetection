e0a6acd6e6cfd963ca2c87534073d271853eb868
chandanachowdary77 <163419001+chandanachowdary77@users.noreply.github.com>
2025-12-28 14:34:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import PrivateRoute from "./components/PrivateRoute";
+
+import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import URLDetection from "./pages/URLDetection";
 import IPAnalysis from "./pages/IPAnalysis";
@@ -9,16 +12,32 @@ import Reports from "./pages/Reports";
 function App() {
   return (
     <Router>
-      <Navbar />
-      <div className="p-6">
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/url-detection" element={<URLDetection />} />
-          <Route path="/ip-analysis" element={<IPAnalysis />} />
-          <Route path="/pcap-analysis" element={<PCAPAnalysis />} />
-          <Route path="/reports" element={<Reports />} />
-        </Routes>
-      </div>
+      <Routes>
+
+        {/* Login */}
+        <Route path="/login" element={<Login />} />
+
+        {/* Protected Routes */}
+        <Route
+          path="/*"
+          element={
+            <PrivateRoute>
+              <>
+                <Navbar />
+                <div className="p-6">
+                  <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/url-detection" element={<URLDetection />} />
+                    <Route path="/ip-analysis" element={<IPAnalysis />} />
+                    <Route path="/pcap-analysis" element={<PCAPAnalysis />} />
+                    <Route path="/reports" element={<Reports />} />
+                  </Routes>
+                </div>
+              </>
+            </PrivateRoute>
+          }
+        />
+      </Routes>
     </Router>
   );
 }
